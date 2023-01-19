a736abc36f0502fd1083c693a663cdd5322d5a41
Ali Omarov <omarovali425@gmail.com>
2023-01-19 22:03:12 +0300

    File changed

--- src/Worker.java
+++ src/Worker.java
@@ -4,27 +4,31 @@ public class Worker {
     public interface OnTaskDoneListener {
         void onDone(String result);
     }
+
     @FunctionalInterface
     public interface OnTaskErrorListener {
         void onError(String result);
     }
-        private OnTaskDoneListener callback;
-        private OnTaskErrorListener errorCallback;
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
 
 
-        public Worker(OnTaskDoneListener callback) {
-            this.callback = callback;
-       }
-    public Worker(OnTaskErrorListener errorCallback) {
-            this.errorCallback = errorCallback;
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
     }
-        public void start() {
-           for (int i = 0; i < 100; i++) {
-                callback.onDone("Task " + i + " is done");
-                if (i == 33) {
-                    errorCallback.onError("Task " + i + " is error");
-                }
+
+    //  public Worker(OnTaskErrorListener errorCallback) {
+    //  this.errorCallback = errorCallback;
+    // }
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            callback.onDone("Task " + i + " is done");
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is error");
             }
         }
+    }
 
 }
